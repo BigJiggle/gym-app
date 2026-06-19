73fc3d2e12b751ba5ddd2a704bc818cb2747014c
BigJiggle <danfinance1212@gmail.com>
2026-06-19 12:07:54 +0000

    [QA] 2026-06-19: enforce meal_count minimum of 3 in generateNutritionPlan

--- electron/services/nutritionEngine.ts
+++ electron/services/nutritionEngine.ts
@@ -683,7 +683,7 @@ export function generateNutritionPlan(input: NutritionInput): NutritionPlan {
     protein_g,
     carbs_g,
     fat_g,
-    input.meal_count,
+    Math.max(3, input.meal_count),
     input.dietary_preference,
     input.food_exclusions,
     input.food_preferences,
